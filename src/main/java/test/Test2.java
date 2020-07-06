64639bb569905943d30b2c16a169761696bd3477
Gayatri <gayatri.aghd@gmail.com>
2020-07-06 17:22:23 +0530

    third commit

--- src/main/java/test/Test2.java
+++ src/main/java/test/Test2.java
@@ -2,4 +2,12 @@ package test;
 
 public class Test2 {
 
+	public void demo(){
+		System.out.println("demo1");
+		System.out.println("");
+	}
+	public void demo1(){
+		System.out.println("demo1");
+	}
+	
 }
